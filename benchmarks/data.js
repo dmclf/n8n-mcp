fd8ea6ddb6050c71a2b7647d02d424040c66bab6
github-action-benchmark <github@users.noreply.github.com>
2026-03-19 15:06:04 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for 14962a39b648d0e22d40b7848950b761d458f34c

---
+++ benchmarks/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1773932764666,
+  "repoUrl": "https://github.com/dmclf/n8n-mcp",
+  "entries": {
+    "n8n-mcp Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "56956555+czlonkowski@users.noreply.github.com",
+            "name": "Romuald Członkowski",
+            "username": "czlonkowski"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "14962a39b648d0e22d40b7848950b761d458f34c",
+          "message": "chore: update n8n to 2.12.3 and bump version to 2.37.4 (#647)",
+          "timestamp": "2026-03-18T17:01:25+01:00",
+          "tree_id": "04450f0a5319f53a2e4d31b4e60f4b392308c2b0",
+          "url": "https://github.com/dmclf/n8n-mcp/commit/14962a39b648d0e22d40b7848950b761d458f34c"
+        },
+        "date": 1773932763935,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0136,
+            "range": "0.3096",
+            "unit": "ms",
+            "extra": "73341 ops/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
